76123a1392d44c59c746b65f95cfea64cd35b821
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-07-10 18:43:23 +0300

    optioins: load lists; defaults button

--- defaults.js
+++ defaults.js
@@ -1,8 +1,7 @@
-/* globals chrome */
-
 'use strict';
 
 module.exports = {
+
     favourites: {
 	black: 'BrendanEich',
 	gray: 'wycats',
@@ -20,5 +19,59 @@ module.exports = {
 	blue: '',
 	teal: '',
 	aqua: ''
-    }
+    },
+
+    // blacklist (exact)
+    hostname: [
+	'www.theguardian.com',
+	'medium.com',
+	'nytimes.com',
+	'newyorker.com',
+	'www.slate.com',
+	'www.huffingtonpost.com',
+
+	'www.apple.com',
+	'daringfireball.net',
+	'marco.org'
+    ],
+
+    // blacklist (exact)
+    user: [],
+
+    // whitelist (regexp)
+    title_white: [
+	'ruby',
+	'rails',
+	'javascript',
+	'\bNode(js)?\b'
+    ],
+
+    // blacklist (regexp)
+    title_black: [
+	'haskell',
+	'java',
+	'\\bApple\\b',
+	'\\bIOS(\\d+)?\\b',
+	'Iphone',
+	'\\bIp[ao]d',
+	'\\bMac(book)?\\b',
+	'\\b(Mac\\s+)?OS(\\s+)?X\\b',
+	'\\bIcloud',
+	'\\bItunes',
+	'Python',
+	'Django',
+	'Pycon',
+	'\\bPatent',
+	'Legislation',
+	'Goverment',
+	'Court\\b',
+	'\\bVim\\b',
+	'A/B\\sTest',
+	'Pirate',
+	'Torrent',
+	'App\\.net',
+	'\\bAsp\\.net',
+	'Nokia',
+	'\\bFlash'
+    ]
 }
--- options/main.js
+++ options/main.js
@@ -6,13 +6,15 @@ require("babel-polyfill")
 
 let defaults = require('../defaults')
 let colours = require('../colours')
+let filter = require('../news/filter')
 
-let favourites_get = function() {
+let favourites_get = function(reset) {
     let colourpairs = defaults.favourites
     chrome.storage.local.get('favourites', (val) => {
-	if (val && val.favourites) colourpairs = val.favourites
+	if (!reset && val.favourites) colourpairs = val.favourites
 
 	let tbody = document.querySelector('#favourites table tbody')
+	tbody.innerHTML = ''
 	for (let key in colourpairs) {
 	    let val = colourpairs[key]
 	    let tr = document.createElement('tr')
@@ -39,9 +41,38 @@ let favourites_save = function() {
     })
 }
 
+let lists_get = function(reset) {
+    let lists = defaults
+    chrome.storage.local.get(['hostname', 'user', 'title_white', 'title_black'], (val) => {
+	if (!reset && Object.keys(val).length === 4) lists = val
+
+	let nodes = document.querySelectorAll('textarea')
+	nodes.forEach( (node) => node.value = lists[node.name].join("\n") )
+    })
+}
+
+let lists_save = function() {
+    let nodes = document.querySelectorAll('textarea')
+    nodes.forEach( (node) => node.disabled = true)
+
+    for (let node of nodes) {
+	let r = {}
+	r[node.name] = filter.parseRawData(node.value)
+	chrome.storage.local.set(r, (val) => node.disabled = false )
+    }
+}
+
 
 favourites_get()
+lists_get()
 
 document.querySelector('#btn_save').onclick = function(event) {
     favourites_save()
+    lists_save()
+}
+
+document.querySelector('#btn_defaults').onclick = function(event) {
+    if (!window.confirm('Are you sure?')) return
+    favourites_get(true)
+    lists_get(true)
 }
