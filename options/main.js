4d15f65fec8188b2258799e91cb5e5b8102e141b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2016-07-11 17:26:38 +0300

    options: small refactorings

--- options/main.js
+++ options/main.js
@@ -8,12 +8,10 @@ let defaults = require('../defaults')
 let colours = require('../colours')
 let filter = require('../news/filter')
 
-let favourites_get = function(reset) {
-    let colourpairs = defaults.favourites
+let settings = function(reset) {
     return new Promise( (resolve, _) => {
-	chrome.storage.local.get('favourites', (val) => {
-	    if (!reset && val.favourites) colourpairs = val.favourites
-	    resolve(colourpairs)
+	chrome.storage.local.get(null, (val) => {
+	    resolve( (!reset && val.favourites && val.filters) ? val : defaults)
 	})
     })
 }
@@ -45,16 +43,6 @@ let favourites_save = function() {
     })
 }
 
-let lists_get = function(reset) {
-    let lists = defaults.filters
-    return new Promise( (resolve, _) => {
-	chrome.storage.local.get('filters', (val) => {
-	    if (!reset && val.filters) lists = val.filters
-	    resolve(lists)
-	})
-    })
-}
-
 let lists_set = function(lists) {
     let nodes = document.querySelectorAll('textarea')
     nodes.forEach( (node) => node.value = lists[node.name].join("\n") )
@@ -73,18 +61,11 @@ let lists_save = function() {
     })
 }
 
-let settings2str = function() {
-    return Promise.all([favourites_get(), lists_get()]).then( (val) => {
-	return JSON.stringify({
-	    favourites: val[0],
-	    filters: val[1]
-	})
-    })
-}
 
-
-favourites_get().then( (val) => favourites_set(val) )
-lists_get().then( (val) => lists_set(val) )
+settings().then( (val) => {
+    favourites_set(val.favourites)
+    lists_set(val.filters)
+})
 
 document.querySelector('#btn_save').onclick = function(event) {
     favourites_save()
@@ -93,13 +74,16 @@ document.querySelector('#btn_save').onclick = function(event) {
 
 document.querySelector('#btn_defaults').onclick = function(event) {
     if (!window.confirm('Are you sure?')) return
-    favourites_get(true).then( (val) => favourites_set(val) )
-    lists_get(true).then( (val) => lists_set(val) )
+    settings(true).then( (val) => {
+	favourites_set(val.favourites)
+	lists_set(val.filters)
+    })
 }
 
 document.querySelector('#btn_export').onclick = function(event) {
-    settings2str().then( (settings) => {
-	let blob = new Blob([settings], {type: 'text/plain'})
+    settings().then( (val) => {
+	val = JSON.stringify(val)
+	let blob = new Blob([val], {type: 'text/plain'})
 	let url = URL.createObjectURL(blob)
 
 	let a = document.createElement('a')
